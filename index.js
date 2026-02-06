4306b1bfa08d607e101348741cd55cd2e3bf89cd
mboungounoellie9-del <mboungounoellie9@gmail.com>
2026-02-06 19:14:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function startSession() {
+  alert("Session IA BoostIA lancée (démo)");
+}
+
+function openDashboard() {
+  alert("Dashboard stratégique BoostIA (démo)");
+}
+
+function choosePlan() {
+  alert("Sélection d’offre BoostIA (démo)");
+}
+
+function contactUs() {
+  alert("Contact BoostIA – nous reviendrons vers vous");
+}
